5e43710efa10a07d540c1defb660e94ff6997a66
endurance21 <43696525+endurance21@users.noreply.github.com>
2018-12-03 02:01:00 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,54 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>REGISTER </title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+		 
+</head>
+
+
+<body>
+            <div style="margin: auto; background-color:grey;">
+                                           <center> REGISTRATION PAGE</center>
+
+ <form action="register.php" method="post">
+
+       <label>username:</label><br>
+    
+            <input type="text" class="inputvalues" placeholder=" username"/><br>
+
+         
+       <label>password:</label><br>
+ 
+
+            <input type"text" class="inputvalues" placeholder="  password"/><br>
+
+        <label> confirm password:</label><br>
+
+           
+              <input type"text" class="inputvalues" placeholder="confirm password"/><br>
+           
+          <input   type="button"  value="register"/> </br>
+     <input type="button"  value="back">
+
+
+</form>
+
+</div>
+
+           
+      
+    
+ 
+
+
+  
+
+     
+	 
+</body>
+</noscript>
+<div style="text-align: center;"><div style="position:relative; top:0; margin-right:auto;margin-left:auto; z-index:99999">
+
+</div></div>
+</html>
